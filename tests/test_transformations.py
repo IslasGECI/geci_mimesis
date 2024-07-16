e86c5148faa254a745855a45e2978c70b618c66d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-16 20:10:21 +0000

    🛑🧪 Fail tests

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -24,3 +24,34 @@ field: dict = {
 def test_number_field():
     obtained = dt.number_field(field)
     assert obtained < 1.67
+
+
+island: dict = {
+    "type": "string",
+    "name": "Isla",
+    "format": "default",
+    "constraints": {
+        "enum": [
+            "Asuncion",
+            "Coronado Medio",
+            "Coronado Norte",
+            "Coronado Sur",
+            "Coronado Terron de Azucar",
+            "Natividad",
+            "San Benito Este",
+            "San Benito Medio",
+            "San Benito Oeste",
+            "San Jeronimo",
+            "San Martin",
+            "San Roque",
+            "Todos Santos",
+            "Todos Santos Norte",
+            "Todos Santos Sur",
+        ]
+    },
+}
+
+
+def test_enum_field():
+    obtained = dt.enum_field(field)
+    assert obtained == "Todos Santos Norte"
