823a7dbcd528cf43a8ba789438bd9b1d8f5a97f7
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-16 20:03:29 +0000

    ✅ Pass tests

--- geci_mimesis/transformations.py
+++ geci_mimesis/transformations.py
@@ -1,6 +1,10 @@
+from mimesis import Field
+
+
 def add_offset(augend: int, addend: int) -> int:
     return augend + addend
 
 
 def number_field(field: dict) -> list:
-    return [1.0, 1.1, 1.3]
+    field = Field()
+    return field("numeric.float_number", start=0.0, end=1.67)
