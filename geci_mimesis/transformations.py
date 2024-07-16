a468a3470133b0b3069b9e91d307f4e74db7ef74
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-16 20:13:48 +0000

    ♻️  Refactor

--- geci_mimesis/transformations.py
+++ geci_mimesis/transformations.py
@@ -1,10 +1,6 @@
 from mimesis import Field
 
 
-def add_offset(augend: int, addend: int) -> int:
-    return augend + addend
-
-
 def number_field(field: dict) -> list:
     field = Field()
     return field("numeric.float_number", start=0.0, end=1.67)
--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -2,14 +2,6 @@ import numpy as np
 import geci_mimesis as dt
 
 
-def test_add_offset():
-    augend = 1
-    addend = 2
-    expected = augend + addend
-    obtained = dt.add_offset(augend, addend)
-    assert expected == obtained
-
-
 field: dict = {
     "name": "Horas_traslado",
     "type": "number",
