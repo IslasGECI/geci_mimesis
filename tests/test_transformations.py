11ee0a6121dd7a56dd31ed5ac0590a4f6a86a46f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-16 18:21:13 +0000

    🛑🧪 Fail tests

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -7,3 +7,18 @@ def test_add_offset():
     expected = augend + addend
     obtained = dt.add_offset(augend, addend)
     assert expected == obtained
+
+
+field: dict = {
+    "name": "Horas_traslado",
+    "type": "number",
+    "format": "default",
+    "long_name": "Commuting time",
+    "nombre_largo": "Tiempo de traslado",
+    "units": "hr",
+    "constraints": {"minimum": 0, "maximum": 1.67},
+}
+
+
+def test_number_column():
+    dt.number_column(field)
