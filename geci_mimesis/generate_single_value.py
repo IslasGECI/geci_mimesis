d3d4c580129193e44a15937283f62d23b252ffcf
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-17 21:28:54 +0000

    ♻️  Refactor

--- geci_mimesis/generate_single_value.py
+++ geci_mimesis/generate_single_value.py
@@ -6,6 +6,28 @@ def number_field(field: dict) -> list:
     return field_generator("numeric.float_number", start=0.0, end=1.67)
 
 
+def get_right_field(field: dict) -> list:
+    right_field = _selector_type_of_field(field)
+    return right_field(field)
+
+
+def _selector_type_of_field(field: dict):
+    field_type: str = _get_right_key(field)
+    different_field = {
+        "integer_constraints": _integer_field,
+        "integer": _integer_field_without_constraints,
+        "string_constraints": _enum_field,
+    }
+    return different_field[field_type]
+
+
+def _get_right_key(field: dict) -> str:
+    field_type: str = field["type"]
+    if "constraints" in field:
+        return f"{field_type}_constraints"
+    return field_type
+
+
 def _integer_field(field: dict) -> list:
     constraints: dict = field["constraints"]
     field_generator = Field()
@@ -23,25 +45,3 @@ def _enum_field(field: dict) -> list:
 def _integer_field_without_constraints(field: dict) -> list:
     field_generator = Field()
     return field_generator("numeric.integer_number")
-
-
-def get_right_field(field: dict) -> list:
-    right_field = _selector_type_of_field(field)
-    return right_field(field)
-
-
-def _get_right_key(field: dict) -> str:
-    field_type: str = field["type"]
-    if "constraints" in field:
-        return f"{field_type}_constraints"
-    return field_type
-
-
-def _selector_type_of_field(field: dict):
-    field_type: str = _get_right_key(field)
-    different_field = {
-        "integer_constraints": _integer_field,
-        "integer": _integer_field_without_constraints,
-        "string_constraints": _enum_field,
-    }
-    return different_field[field_type]
